2faa7400cf81504a1ec1fc0e8c542a854efbf2f2
Erin Hinson <erinhinson@Erins-MacBook-Pro.local>
2019-03-14 20:36:03 -0700

    add props to story

--- packages/component-library/stories/Erin.story.js
+++ packages/component-library/stories/Erin.story.js
@@ -4,6 +4,8 @@ import { number, withKnobs } from '@storybook/addon-knobs';
 import { checkA11y } from '@storybook/addon-a11y';
 import { Erin } from '../src';
 
+const list = ['four', 'five', 'six'];
+
 export default () =>
   storiesOf('Erin', module)
-    .add('Erin', () => <Erin/>);
+    .add('Erin', () => <Erin listItems={list} />);
