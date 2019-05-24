88ad78b918904dec8268950140c8baaf57873187
Tanvi Garg <tanvi.garg23@gmail.com>
2019-05-24 15:13:03 -0700

    latest commit for transpo card

--- packages/2018-transportation-systems/src/components/DiveDeeperIntoTransportationData/index.js
+++ packages/2018-transportation-systems/src/components/DiveDeeperIntoTransportationData/index.js
@@ -5,8 +5,6 @@ import { connect } from "react-redux";
 
 import {
   CivicStoryCard,
-  Placeholder,
-  BaseMap,
   HeatMap,
   IconMap,
   Slider
@@ -44,6 +42,170 @@ const mapGLOptions = {
   keyboard: false
 };
 
+const dataProperty = "new_units";
+const dataMin = 0;
+const dataMax = 2;
+
+const maxZoom = 19;
+
+const startZoomTransition = 11;
+const endZoomTransition = 15;
+
+const heatZoomFadeStart = 14;
+const heatZoomFadeOut = 15;
+const circleZoomFadeEnd = 15;
+const circleZoomFadeIn = 14;
+
+const colorOptions = {
+  Plasma: [
+    0,
+    "rgba(0,0,0,0)",
+    0.2,
+    "#6a00a8",
+    0.4,
+    "#b12a90",
+    0.6,
+    "#e16462",
+    0.8,
+    "#fca636",
+    1,
+    "#f0f921"
+  ],
+  Viridis: [
+    0,
+    "rgba(0,0,0,0)",
+    0.2,
+    "#414487",
+    0.4,
+    "#2a788e",
+    0.6,
+    "#22a884",
+    0.8,
+    "#7ad151",
+    1,
+    "#fde725"
+  ],
+  Inferno: [
+    0,
+    "rgba(0,0,0,0)",
+    0.2,
+    "#420a68",
+    0.4,
+    "#932667",
+    0.6,
+    "#dd513a",
+    0.8,
+    "#fca50a",
+    1,
+    "#fcffa4"
+  ],
+  Magma: [
+    0,
+    "rgba(0,0,0,0)",
+    0.2,
+    "#3b0f70",
+    0.4,
+    "#8c2981",
+    0.6,
+    "#de4968",
+    0.8,
+    "#fe9f6d",
+    1,
+    "#fcfdbf"
+  ],
+  Warm: [
+    0,
+    "rgba(0,0,0,0)",
+    0.2,
+    "rgb(191, 60, 175)",
+    0.4,
+    "rgb(254, 75, 131)",
+    0.6,
+    "rgb(255, 120, 71)",
+    0.8,
+    "rgb(226, 183, 47)",
+    1,
+    "rgb(175, 240, 91)"
+  ],
+  Cool: [
+    0,
+    "rgba(0,0,0,0)",
+    0.2,
+    "rgb(76, 110, 219)",
+    0.4,
+    "rgb(35, 171, 216)",
+    0.6,
+    "rgb(29, 223, 163)",
+    0.8,
+    "rgb(82, 246, 103)",
+    1,
+    "rgb(175, 240, 91)"
+  ]
+};
+
+const heatMapColorScale = [
+  "interpolate",
+  ["linear"],
+  ["heatmap-density"],
+  colorOptions
+];
+
+const heatMapRadius = [
+  "interpolate",
+  ["linear"],
+  ["zoom"],
+  startZoomTransition,
+  15,
+  endZoomTransition,
+  20
+];
+
+const heatMapOpacity = [
+  "interpolate",
+  ["linear"],
+  ["zoom"],
+  heatZoomFadeStart,
+  1,
+  heatZoomFadeOut,
+  0
+];
+
+const circleRadius = [
+  "interpolate",
+  ["linear"],
+  ["zoom"],
+  circleZoomFadeEnd,
+  ["interpolate", ["linear"], ["get", dataProperty], dataMin, 1, dataMax, 10],
+  maxZoom,
+  ["interpolate", ["linear"], ["get", dataProperty], dataMin, 11, dataMax, 20]
+];
+
+const circleOpacity = [
+  "interpolate",
+  ["linear"],
+  ["zoom"],
+  circleZoomFadeIn,
+  0,
+  circleZoomFadeEnd,
+  1
+];
+
+const circleFillColor = colorOptions["Plasma"];
+
+const circleStrokeColor = colorOptions["Warm"];
+
+const circleStrokeWidth = 1.5;
+
+const circleStrokeOpacity = [
+  "interpolate",
+  ["linear"],
+  ["zoom"],
+  circleZoomFadeIn,
+  0,
+  circleZoomFadeEnd,
+  1
+];
+
 export class DiveDeeperIntoTransportationData extends React.Component {
   componentDidMount() {
     this.props.init();
@@ -59,10 +221,21 @@ export class DiveDeeperIntoTransportationData extends React.Component {
         loading={isLoading}
         error={error && "Error loading data"}
       >
-        <BaseMap
-          mapGLOptions={mapGLOptions}
-          initialZoom={ZOOM}
-          navigation={false}
+        <HeatMap
+          centerLatitude={45.5597}
+          centerLongitude={-122.7066}
+          initialZoom={9.75}
+          maxZoom={maxZoom}
+          mapStyle="mapbox://styles/hackoregon/cjiazbo185eib2srytwzleplg"
+          heatMapColorScale={heatMapColorScale}
+          heatMapRadius={heatMapRadius}
+          heatMapOpacity={heatMapOpacity}
+          circleRadius={circleRadius}
+          circleOpacity={circleOpacity}
+          circleFillColor={circleFillColor}
+          circleStrokeColor={circleStrokeColor}
+          circleStrokeWidth={circleStrokeWidth}
+          circleStrokeOpacity={circleStrokeOpacity}
         >
           {data && (
             <IconMap
@@ -76,7 +249,7 @@ export class DiveDeeperIntoTransportationData extends React.Component {
               highlightColor={[0, 0, 0, 0]}
             />
           )}
-        </BaseMap>
+        </HeatMap>
         <Slider value={5} min={0} max={10} />
       </CivicStoryCard>
     );
