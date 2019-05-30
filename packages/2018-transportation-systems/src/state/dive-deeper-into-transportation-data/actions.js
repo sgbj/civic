160fcefdb1f3be0f17cae895f0c64c2afa86a96f
Tanvi Garg <tanvi.garg23@gmail.com>
2019-05-29 20:43:36 -0700

    add fetch traffic light action types, api actions and thunk actions

--- packages/2018-transportation-systems/src/state/dive-deeper-into-transportation-data/actions.js
+++ packages/2018-transportation-systems/src/state/dive-deeper-into-transportation-data/actions.js
@@ -1,10 +1,16 @@
 import fetchAdapter from "../fetch-adapter";
 import actionEmitter from "../api-adapter-action-emitter";
 
+// Types
 export const API_START = "DIVE_DEEPER_INTO_TRANSPORTATION_DATA/START";
 export const API_SUCCESS = "DIVE_DEEPER_INTO_TRANSPORTATION_DATA/SUCCESS";
 export const API_FAILURE = "DIVE_DEEPER_INTO_TRANSPORTATION_DATA/ERROR";
 
+export const TRAFFIC_LIGHT_API_START = "TRAFFIC_LIGHT_DATA/START";
+export const TRAFFIC_LIGHT_API_SUCCESS = "TRAFFIC_LIGHT_DATA/SUCCESS";
+export const TRAFFIC_LIGHT_API_FAILURE = "TRAFFIC_LIGHT_DATA/ERROR";
+
+// Simple actions
 export const diveDeeperIntoTransportationDataStart = actionEmitter(API_START);
 export const diveDeeperIntoTransportationDataSuccess = actionEmitter(
   API_SUCCESS
@@ -13,11 +19,25 @@ export const diveDeeperIntoTransportationDataFailure = actionEmitter(
   API_FAILURE
 );
 
+export const trafficLightDataStart = actionEmitter(TRAFFIC_LIGHT_API_START);
+export const trafficLightDataSuccess = actionEmitter(TRAFFIC_LIGHT_API_SUCCESS);
+export const trafficLightDataFailure = actionEmitter(TRAFFIC_LIGHT_API_FAILURE);
+
+// Thunk actions
 export const fetchDiveDeeperIntoTransportationData = fetchAdapter(
   `trimet-stop-events/disturbance-stops/`,
   {
     start: diveDeeperIntoTransportationDataStart,
     success: diveDeeperIntoTransportationDataSuccess,
     failure: diveDeeperIntoTransportationDataFailure
+  }
+);
+
+export const fetchTrafficLightData = fetchAdapter(
+  `/totalcontributionsrawmonthtotal/?limit=300`,
+  {
+    start: trafficLightDataStart,
+    success: trafficLightDataSuccess,
+    failure: trafficLightDataFailure
   }
 );
