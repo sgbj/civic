cf87e45a392b0d1360e19ff05cf8faa1bec4399a
Kelly de Vries <kdv@me.com>
2019-06-12 11:57:48 -0700

    WIP - added initial changes to create knobs etc
    - removed unused code
    - updated options format
    
    - reached out to @bradfordjanice for help

--- packages/component-library/stories/DropdownMenu.story.js
+++ packages/component-library/stories/DropdownMenu.story.js
@@ -3,44 +3,90 @@ import React from "react";
 /* eslint-disable import/no-extraneous-dependencies */
 import { storiesOf } from "@storybook/react";
 import { checkA11y } from "@storybook/addon-a11y";
+import {
+  withKnobs,
+  text,
+  object,
+  boolean
+  // optionsKnob as options // QUESTION- I don't understand this
+} from "@storybook/addon-knobs";
+
 import { Dropdown } from "../src";
-import { storybookStyles } from "./storyStyles";
 
-const title = "Simple usage";
-
-const demoCode = () => (
-  <Dropdown
-    dispatch={dispatch => action => dispatch(action)}
-    reduxAction={payload => console.log({ type: "ACTION", payload })}
-    options={[
-      { value: "0", label: "David" },
-      { value: "1", label: "Daniel" },
-      { value: "2", label: "Last" },
-      { value: "3", label: "Name" }
-    ]}
-  />
-);
+const GROUP_IDS = {
+  LABELS: "Labels",
+  DATA: "Data",
+  CUSTOM: "Custom"
+};
 
 export default () =>
   storiesOf("Component Lib|Basic Inputs/Dropdown List", module)
     .addDecorator(checkA11y)
-    .addDecorator(story => (
-      <div style={storybookStyles.storyGrid}>
-        <div style={storybookStyles.storyGridItem}>{story()}</div>
-      </div>
-    ))
-    .add("Simple usage", () => {
+
+    .addDecorator(withKnobs)
+    .add("Standard", () => {
+      const clearable = boolean("Clearable", false);
+      const searchable = boolean("Searchable", true);
+      const disabled = boolean("Disabled", false);
+      const defaultValue = {
+        label: "Murphy"
+      };
       const options = [
-        { value: "0", label: "Murphy" },
-        { value: "1", label: "Carter" },
-        { value: "2", label: "Bebe" },
-        { value: "3", label: "Cissi" }
+        { label: "Murphy" },
+        { label: "Carter" },
+        { label: "Bebe" },
+        { label: "Cissi" }
       ];
+
+      const data = object(defaultValue, GROUP_IDS);
+
       return (
         <Dropdown
+          data={data}
+          options={options}
+          clearable={clearable}
+          searchable={searchable}
+          disabled={disabled}
           dispatch={dispatch => action => dispatch(action)}
           reduxAction={payload => console.log({ type: "ACTION", payload })}
+        />
+      );
+    })
+    .add("Custom", () => {
+      const clearable = boolean("Clearable", false);
+      const searchable = boolean("Searchable", true);
+      const disabled = boolean("Disabled", false);
+      // const onChange = func();
+      // const value = any();
+      const simpleValue = boolean();
+      const multi = boolean();
+      const placeholder = text();
+      // const className?
+      const defaultValue = {
+        label: "Murphy"
+      };
+      const options = [
+        { label: "Murphy" },
+        { label: "Carter" },
+        { label: "Bebe" },
+        { label: "Cissi" }
+      ];
+      const data = object(defaultValue, GROUP_IDS);
+
+      return (
+        <Dropdown
+          data={data}
+          clearable={clearable}
+          searchable={searchable}
+          disabled={disabled}
+          // dispatch={dispatch => action => dispatch(action)}
+          // reduxAction={payload => console.log({ type: "ACTION", payload })}
           options={options}
+          // onChange={onChange}
+          // value={value}
+          simpleValue={simpleValue}
+          multi={multi}
+          placeholder={placeholder}
         />
       );
     });
