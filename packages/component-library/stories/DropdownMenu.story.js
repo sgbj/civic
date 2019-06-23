e87489d7ea3c6a0f6b0696ae6c069499a179b078
Kelly de Vries <kdv@me.com>
2019-06-22 17:32:35 -0700

    update options to "real" data, remove custom, & fix notes file name

--- packages/component-library/stories/DropdownMenu.story.js
+++ packages/component-library/stories/DropdownMenu.story.js
@@ -3,13 +3,7 @@ import React from "react";
 /* eslint-disable import/no-extraneous-dependencies */
 import { storiesOf } from "@storybook/react";
 import { checkA11y } from "@storybook/addon-a11y";
-import {
-  withKnobs,
-  text,
-  object,
-  boolean
-  // optionsKnob as options // QUESTION- I don't understand this
-} from "@storybook/addon-knobs";
+import { withKnobs, object, boolean } from "@storybook/addon-knobs";
 
 import { Dropdown } from "../src";
 
@@ -22,23 +16,21 @@ const GROUP_IDS = {
 export default () =>
   storiesOf("Component Lib|Basic Inputs/Dropdown List", module)
     .addDecorator(checkA11y)
-
     .addDecorator(withKnobs)
     .add("Standard", () => {
       const clearable = boolean("Clearable", false);
       const searchable = boolean("Searchable", true);
       const disabled = boolean("Disabled", false);
-      const defaultValue = {
-        label: "Murphy"
-      };
+
       const options = [
-        { label: "Murphy" },
-        { label: "Carter" },
-        { label: "Bebe" },
-        { label: "Cissi" }
+        { value: "0", label: "Statewide Races" },
+        { value: "1", label: "Statewide Ballot Measure" },
+        { value: "2", label: "Portland and Multnomah County" },
+        { value: "3", label: "State Legislature" },
+        { value: "4", label: "Other" }
       ];
 
-      const data = object(defaultValue, GROUP_IDS);
+      const data = object("Data", options, GROUP_IDS.DATA);
 
       return (
         <Dropdown
@@ -49,44 +41,6 @@ export default () =>
           disabled={disabled}
           dispatch={dispatch => action => dispatch(action)}
           reduxAction={payload => console.log({ type: "ACTION", payload })}
-        />
-      );
-    })
-    .add("Custom", () => {
-      const clearable = boolean("Clearable", false);
-      const searchable = boolean("Searchable", true);
-      const disabled = boolean("Disabled", false);
-      // const onChange = func();
-      // const value = any();
-      const simpleValue = boolean();
-      const multi = boolean();
-      const placeholder = text();
-      // const className?
-      const defaultValue = {
-        label: "Murphy"
-      };
-      const options = [
-        { label: "Murphy" },
-        { label: "Carter" },
-        { label: "Bebe" },
-        { label: "Cissi" }
-      ];
-      const data = object(defaultValue, GROUP_IDS);
-
-      return (
-        <Dropdown
-          data={data}
-          clearable={clearable}
-          searchable={searchable}
-          disabled={disabled}
-          // dispatch={dispatch => action => dispatch(action)}
-          // reduxAction={payload => console.log({ type: "ACTION", payload })}
-          options={options}
-          // onChange={onChange}
-          // value={value}
-          simpleValue={simpleValue}
-          multi={multi}
-          placeholder={placeholder}
         />
       );
     });
