589e912f37084cdfc8b96b179632717e7fdf0430
Kelly de Vries <kdv@me.com>
2019-06-27 18:28:16 -0700

    add StatefulWrapper component & addon-actions

--- packages/component-library/stories/DropdownMenu.story.js
+++ packages/component-library/stories/DropdownMenu.story.js
@@ -3,15 +3,10 @@ import React from "react";
 /* eslint-disable import/no-extraneous-dependencies */
 import { storiesOf } from "@storybook/react";
 import { checkA11y } from "@storybook/addon-a11y";
-import {
-  withKnobs,
-  object,
-  boolean,
-  text,
-  select
-} from "@storybook/addon-knobs";
-// import { action } from "@storybook/addon-actions";
-// import Select from "react-select";
+import { action } from "@storybook/addon-actions";
+
+import { withKnobs, object, boolean, text } from "@storybook/addon-knobs";
+import StatefulWrapper from "../src/utils/StatefulWrapper";
 import { Dropdown } from "../src";
 
 const GROUP_IDS = {
@@ -28,11 +23,9 @@ export default () =>
       const clearable = boolean("Clearable", false);
       const searchable = boolean("Searchable", true);
       const disabled = boolean("Disabled", false);
-      // const value = options("value here", true) // HELP? should be populated by select option - HOW??
 
       const placeholder = text("Select");
       const simpleValue = boolean("Simple Value", true);
-      const label = "Select data";
       const data = [
         { value: "0", label: "Statewide Races" },
         { value: "1", label: "Statewide Ballot Measure" },
@@ -42,20 +35,26 @@ export default () =>
       ];
 
       const options = object("Data", data, GROUP_IDS.DATA);
-      const defaultValue = "Select";
-      const value = select(label, options, defaultValue, GROUP_IDS.DATA);
 
       return (
-        <Dropdown
-          options={options}
-          onChange={() => console.log("onChange", data)}
-          value={value}
-          clearable={clearable}
-          searchable={searchable}
-          disabled={disabled}
-          placeholder={placeholder}
-          simpleValue={simpleValue}
-          reduxAction={payload => console.log({ type: "ACTION", payload })}
-        />
+        <StatefulWrapper initialState={{ value: 0 }}>
+          {({ get, set }) => {
+            return (
+              <Dropdown
+                options={options}
+                onChange={value => {
+                  set({ value });
+                  action("onChange")(value);
+                }}
+                value={get("value")}
+                clearable={clearable}
+                searchable={searchable}
+                disabled={disabled}
+                placeholder={placeholder}
+                simpleValue={simpleValue}
+              />
+            );
+          }}
+        </StatefulWrapper>
       );
     });
